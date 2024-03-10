3f862487d8eaa9f316cf16107550d3341ddde44a
Alig8 <91729236+Alig8@users.noreply.github.com>
2024-03-11 00:51:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.on('ready', async () => {
     .setName('Garena Free Fire')
     .setDetails('iPhone 13 Pro Max')
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('mp:http://attachments/1086722226751013004/1216502186851172493/1001710092707.png?ex=66009ef2&is=65ee29f2&hm=0167468ad717bad51a941b6a6171833e8c87367c6c2b4754ae05b16d29f98ca8&') //ảnh lớn (dạng.png hoặc .gif)
+    .setAssetsLargeImage('mp:attachments/1086722226751013004/1216502186851172493/1001710092707.png?ex=66009ef2&is=65ee29f2&hm=0167468ad717bad51a941b6a6171833e8c87367c6c2b4754ae05b16d29f98ca8&') //ảnh lớn (dạng.png hoặc .gif)
     //.setAssetsLargeText('FFicone')
     //.addButton('nameyourbutton1', 'https://discord.gg/')
     //.addButton('nameyourbutton2', 'https://www.facebook.com/')
